38f2fd3de328d15cf6a41555a583a542dab8af7c
David Hicks <davicks@live.unc.edu>
2020-12-24 20:53:05 -0500

    Updated typeahead to async-typeahead and commented

--- dailymed-ui/src/components/RxTermsSearch/index.jsx
+++ dailymed-ui/src/components/RxTermsSearch/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Typeahead } from 'react-bootstrap-typeahead';
+import { AsyncTypeahead, Typeahead } from "react-bootstrap-typeahead";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
@@ -11,62 +11,28 @@ import 'react-bootstrap-typeahead/css/Typeahead.css';
 export default class RxTermsSearch extends React.Component {
     constructor(props) {
         super(props);
-
+        /** State object for the typeahead */
         this.state = {
-            settings: {
-                drugNameMinLength: 3,
-                highlightOnlyResult: true,
-                paginate: true,
-                maxResults: 7,   
+          settings: {
+            drugNameMinLength: 3,
+            highlightOnlyResult: true,
+            paginate: true,
+            maxResults: 7,
+          },
+          options: {
+            drugName: [],
+            drugStrengthAndForm: [],
+          },
+          selected: {
+            drugName: "",
+            drugStrengthAndForm: "",
+            rxcui: "",
             },
-            options: {
-                drugName: [],
-                drugStrengthAndForm: [],
-            },
-            selected: {
-                drugName: '',
-                drugStrengthAndForm: '',
-                rxcui: '',
-            }
+            isLoading: false,
         };
     }
 
-    handleDrugNameChange(text) {
-        const { settings, options } = this.state;
-
-        if (text.length >= settings.drugNameMinLength) {
-            fetch(`https://clinicaltables.nlm.nih.gov/api/rxterms/v3/search?ef=DISPLAY_NAME,STRENGTHS_AND_FORMS,RXCUIS&maxList=&terms=${text}`)
-            .then(resp => resp.json())
-            .then(json => {
-                // NOTE: use total later when customizing the "pagination" display to be more like RxTerms
-                // const total = json[0];
-                const count = json[1].length;
-                const data = json[2];
-                var drugNameOptions = [];
-                
-                // this series of nested loops could probably be optimized
-                for (var i = 0; i < count; i++) {
-                    const displayName = data.DISPLAY_NAME[i];
-                    const strengthsAndForms = data.STRENGTHS_AND_FORMS[i];
-                    const rxcuis = data.RXCUIS[i];
-                    drugNameOptions[i] = {
-                        displayName: displayName,
-                        strengthsAndForms: []
-                    }
-                    for (var j = 0; j < strengthsAndForms.length; j++) {
-                        drugNameOptions[i].strengthsAndForms.push({
-                            strengthAndForm: strengthsAndForms[j],
-                            rxcui: rxcuis[j]
-                        });
-                    }
-                }
-                console.log(drugNameOptions)
-                options.drugName = drugNameOptions;
-                // NOTE: also clear selected when this changes
-                this.setState({ options, selected: [] });
-            });
-        }
-    }
+  
 
     handleDrugStrengthAndFormChange(text) {
         // NOTE: also clear selected when this changes
@@ -82,7 +48,7 @@ export default class RxTermsSearch extends React.Component {
         }
         this.setState({ options, selected });
     }
-
+ 
     setDrugStrengthAndForm(selection) {
         const { selected } = this.state;
 
@@ -97,46 +63,88 @@ export default class RxTermsSearch extends React.Component {
     render() {
         const { settings, options, selected } = this.state;      
         return (
-            <div>
-                <Form.Group>
-                    <Form.Label>Drug name</Form.Label>
-                    <Typeahead
-                        id="drug-name-typeahead"
-                        minLength={settings.drugNameMinLength}
-                        highlightOnlyResult={settings.highlightOnlyResult}
-                        paginate={settings.paginate}
-                        maxResults={settings.maxResults}
-                        labelKey={'displayName'}
-                        onInputChange={(text) => this.handleDrugNameChange(text)}
-                        onChange={(selection) => this.setDrugName(selection)}
-                        options={options.drugName}
-                        placeholder="Enter a drug name"
-                    />
-                    <Form.Text>Minimum {settings.drugNameMinLength} characters.</Form.Text>
-                </Form.Group>
+          <div>
+            <Form.Group>
+              <Form.Label>Drug name</Form.Label>
+              <AsyncTypeahead
+                id="drug-name-typeahead"
+                minLength={settings.drugNameMinLength}
+                highlightOnlyResult={settings.highlightOnlyResult}
+                paginate={settings.paginate}
+                maxResults={settings.maxResults}
+                /**Object property that will be used for the dropdown item */
+                labelKey={"displayName"}
+                /**Function that runs on a selected dropdown option*/
+                onChange={(selection) => this.setDrugName(selection)}
+                /**The dropdown options available for selection*/
+                options={options.drugName}
+                /**Text shown in the dropdown box when a search is being fetched*/
+                searchText="Searching..."
+                placeholder="Enter a drug name..."
+                /**Whether the dropdown should show the loading icon*/
+                isLoading={this.state.isLoading}
+                /**Runs when any text is being typed into the typeahead*/
+                onSearch={(text) => {
+                  this.setState({ isLoading: true });
+                  this.searchForTerms(text, settings, options);
+                }}
+              />
+              <Form.Text>
+                Minimum {settings.drugNameMinLength} characters.
+              </Form.Text>
+            </Form.Group>
 
-                <Form.Group>
-                    <Form.Label>Drug strength and form</Form.Label>
-                    <Typeahead
-                        id="drug-strength-typeahead"
-                        minLength={0}
-                        highlightOnlyResult={settings.highlightOnlyResult}
-                        paginate={settings.paginate}
-                        maxResults={settings.maxResults}
-                        labelKey={'strengthAndForm'}
-                        onInputChange={(text) => this.handleDrugStrengthAndFormChange(text)}
-                        onChange={(selection) => this.setDrugStrengthAndForm(selection)}
-                        options={options.drugStrengthAndForm}
-                        placeholder="Choose a strength and form"
-                    />
-                </Form.Group>
+            <Form.Group>
+              <Form.Label>Drug strength and form</Form.Label>
+              <Typeahead
+                id="drug-strength-typeahead"
+                minLength={0}
+                highlightOnlyResult={settings.highlightOnlyResult}
+                paginate={settings.paginate}
+                maxResults={settings.maxResults}
+                labelKey={"strengthAndForm"}
+                onInputChange={(text) =>
+                  this.handleDrugStrengthAndFormChange(text)
+                }
+                onChange={(selection) => this.setDrugStrengthAndForm(selection)}
+                options={options.drugStrengthAndForm}
+                placeholder="Choose a strength and form"
+              />
+            </Form.Group>
 
-                <div className="float-right">
-                    <Button variant="light">Reset</Button>{' '}
-                    <Button variant="primary" disabled={!selected.rxcui}>Next</Button>{' '}
-                </div>
-
-            </div>      
+            <div className="float-right">
+              <Button variant="light">Reset</Button>{" "}
+              <Button variant="primary" disabled={!selected.rxcui}>
+                Next
+              </Button>{" "}
+            </div>
+          </div>
         );
+    }
+    /** Adds queried drug names and strengths to the typeahead options list and empties the selected array.
+     * @param {string} text - The current text entered into the input
+     * @param {object} settings - The settings for the typeahead
+     * @param {object} options - The options for the typeahead dropdown
+     * 
+    */
+    searchForTerms(text, settings, options) {
+        if (text.length >= settings.drugNameMinLength)
+            fetch(
+                `https://clinicaltables.nlm.nih.gov/api/rxterms/v3/search?ef=DISPLAY_NAME,STRENGTHS_AND_FORMS,RXCUIS&maxList=&terms=${text}`
+            )
+                .then((resp) => resp.json())
+                .then((json) => {
+                    //** Deconstruct the fetch result */
+                    const { DISPLAY_NAME: names, STRENGTHS_AND_FORMS: forms, RXCUIS: cuis } = json[2];
+                    //**If names are returned, set the dropdown options to the returned names */
+                    if (names && forms)
+                        options.drugName = names.map((name, index) => {
+                            return {
+                                displayName: name,
+                                strengthsAndForms: forms[index]
+                            };
+                        });
+                    this.setState({ isLoading: false, options, selected: [] });
+                });
     }
 }
