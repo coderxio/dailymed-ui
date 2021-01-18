481519525f7517c88b2f8b791be95fed5d806afe
Joey LeGrand <jrlegrand@gmail.com>
2021-01-18 19:56:31 +0000

    DailyMed API integration

--- dailymed-ui/src/components/RxTermsSearch/index.jsx
+++ dailymed-ui/src/components/RxTermsSearch/index.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 import { AsyncTypeahead, Typeahead } from "react-bootstrap-typeahead";
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 
 export default class RxTermsSearch extends React.Component {
@@ -28,15 +26,55 @@ export default class RxTermsSearch extends React.Component {
           drugStrengthAndForm: "",
           rxcui: "",
           },
-          isLoading: false,
+          loading: false,
       };
   }
 
+  handleFilterChange() {
+    const { selected } = this.state;
+    this.props.onFilterChange({ 'rxcui': selected.rxcui });
+  }
 
+  /** Adds queried drug names and strengths to the typeahead options list and empties the selected array.
+   * @param {string} text - The current text entered into the input
+  */
+ handleDrugNameChange(text) {
+  this.setState({ loading: true });
+  const { settings, options } = this.state;
 
-  handleDrugStrengthAndFormChange(text) {
+  if (text.length >= settings.drugNameMinLength) {
+    fetch(
+        `https://clinicaltables.nlm.nih.gov/api/rxterms/v3/search?ef=DISPLAY_NAME,STRENGTHS_AND_FORMS,RXCUIS&maxList=&terms=${text}`
+    )
+    .then((resp) => resp.json())
+    .then((json) => {
+        //** Deconstruct the fetch result */
+        const { DISPLAY_NAME: names, STRENGTHS_AND_FORMS: forms, RXCUIS: cuis } = json[2];
+        //**If names are returned, set the dropdown options to the returned names */
+        if (names && forms) 
+          options.drugName = names.map((name, nameIndex) => {
+              return {
+                  displayName: name,
+                  strengthsAndForms: forms[nameIndex].map((form, formIndex) => {
+                    return {
+                      strengthAndForm: form,
+                      rxcui: cuis[nameIndex][formIndex]
+                    };
+                  })
+              };
+          });
+
+          const loading = false;
+          const selected = [];
+          this.setState({ loading, options, selected }, this.handleFilterChange);
+    });
+  }
+}
+
+handleDrugStrengthAndFormChange(text) {
       // NOTE: also clear selected when this changes
-      this.setState({ selected: [] });       
+      const selected = [];
+      this.setState({ selected }, this.handleFilterChange);
   }
 
   setDrugName(selection) {
@@ -46,7 +84,7 @@ export default class RxTermsSearch extends React.Component {
           selected.drugName = selection[0].displayName;
           options.drugStrengthAndForm = selection[0].strengthsAndForms;
       }
-      this.setState({ options, selected });
+      this.setState({ options, selected }, this.handleFilterChange);
   }
 
   setDrugStrengthAndForm(selection) {
@@ -56,45 +94,11 @@ export default class RxTermsSearch extends React.Component {
           selected.drugStrengthAndForm = selection[0].strengthAndForm;
           selected.rxcui = selection[0].rxcui;
       }
-      this.setState({ selected });
+      this.setState({ selected }, this.handleFilterChange);
   }
 
-  /** Adds queried drug names and strengths to the typeahead options list and empties the selected array.
-   * @param {string} text - The current text entered into the input
-   * @param {object} settings - The settings for the typeahead
-   * @param {object} options - The options for the typeahead dropdown
-   * 
-  */
-  searchForTerms(text, settings, options) {
-    if (text.length >= settings.drugNameMinLength) {
-      fetch(
-          `https://clinicaltables.nlm.nih.gov/api/rxterms/v3/search?ef=DISPLAY_NAME,STRENGTHS_AND_FORMS,RXCUIS&maxList=&terms=${text}`
-      )
-      .then((resp) => resp.json())
-      .then((json) => {
-          //** Deconstruct the fetch result */
-          const { DISPLAY_NAME: names, STRENGTHS_AND_FORMS: forms, RXCUIS: cuis } = json[2];
-          //**If names are returned, set the dropdown options to the returned names */
-          if (names && forms)
-              options.drugName = names.map((name, nameIndex) => {
-                  return {
-                      displayName: name,
-                      strengthsAndForms: forms[nameIndex].map((form, formIndex) => {
-                        return {
-                          strengthAndForm: form,
-                          rxcui: cuis[nameIndex][formIndex]
-                        };
-                      })
-                  };
-              });
-
-          this.setState({ isLoading: false, options, selected: [] });
-      });
-    }
-  }
-
-render() {
-        const { settings, options, selected } = this.state;
+  render() {
+        const { settings, options, loading } = this.state;
         return (
           <div>
             <Form.Group>
@@ -102,25 +106,21 @@ render() {
               <AsyncTypeahead
                 id="drug-name-typeahead"
                 labelKey={"displayName"}
+                // The dropdown options available for selection
+                options={options.drugName}
                 minLength={settings.drugNameMinLength}
                 highlightOnlyResult={settings.highlightOnlyResult}
-                paginate={settings.paginate}
                 maxResults={settings.maxResults}
-                //Object property that will be used for the dropdown item                 labelKey={"displayName"}
-                //Function that runs on a selected dropdown option
-                onChange={(selection) => this.setDrugName(selection)}
-                //The dropdown options available for selection
-                options={options.drugName}
-                //Text shown in the dropdown box when a search is being fetched
-                searchText="Searching..."
+                paginate={settings.paginate}
                 placeholder="Enter a drug name..."
-                //Whether the dropdown should show the loading icon
-                isLoading={this.state.isLoading}
-                //Runs when any text is being typed into the typeahead
-                onSearch={(text) => {
-                  this.setState({ isLoading: true });
-                  this.searchForTerms(text, settings, options);
-                }}
+                // Text shown in the dropdown box when a search is being fetched
+                searchText="Searching..."
+                // Whether the dropdown should show the loading icon
+                isLoading={loading}
+                // Runs when any text is being typed into the typeahead
+                onSearch={(text) => this.handleDrugNameChange(text)}
+                // Function that runs on a selected dropdown option
+                onChange={(selection) => this.setDrugName(selection)}
               />
               <Form.Text>
                 Minimum {settings.drugNameMinLength} characters.
@@ -131,26 +131,17 @@ render() {
               <Form.Label>Drug strength and form</Form.Label>
               <Typeahead
                 id="drug-strength-typeahead"
+                labelKey={"strengthAndForm"}
+                options={options.drugStrengthAndForm}
                 minLength={0}
                 highlightOnlyResult={settings.highlightOnlyResult}
-                paginate={settings.paginate}
                 maxResults={settings.maxResults}
-                labelKey={"strengthAndForm"}
-                onInputChange={(text) =>
-                  this.handleDrugStrengthAndFormChange(text)
-                }
-                onChange={(selection) => this.setDrugStrengthAndForm(selection)}
-                options={options.drugStrengthAndForm}
+                paginate={settings.paginate}
                 placeholder="Choose a strength and form..."
+                onInputChange={(text) => this.handleDrugStrengthAndFormChange(text)}
+                onChange={(selection) => this.setDrugStrengthAndForm(selection)}
               />
             </Form.Group>
-
-            <div className="float-right">
-              <Button variant="light">Reset</Button>{" "}
-              <Button variant="primary" disabled={!selected.rxcui}>
-                Next
-              </Button>{" "}
-            </div>
           </div>
         );
     }
